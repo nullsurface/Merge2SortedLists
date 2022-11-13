ad0715e52c9c7326cbf6e39016f652b3cae373fd
Andrew <nullsurface@github.com>
2022-11-12 19:40:47 -0800

    Implemented

--- src/list.c
+++ src/list.c
@@ -1,8 +1,40 @@
 #include "list.h"
-#include <stdio.h> 
+#include <stdlib.h> 
+#include <stdio.h>
 
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
-    ListNode* merged_list;
+    ListNode* merged_list = malloc(sizeof(ListNode));
+    ListNode* curr_node = merged_list;
+
+    while (list1 && list2) {
+        if (list1->val < list2->val) {
+            curr_node->next = malloc(sizeof(ListNode));
+            curr_node->val = list1->val;
+            curr_node->next->val = list2->val;
+        } else {
+            curr_node->next = malloc(sizeof(ListNode));
+            curr_node->val = list2->val;
+            curr_node->next->val = list1->val;
+        }
+        
+        curr_node->next->next = malloc(sizeof(ListNode));
+        curr_node = curr_node->next->next;
+        list1 = list1->next;
+        list2 = list2->next;
+    }
+
+    while(list1) {
+        curr_node->val = list1->val;
+        curr_node->next = malloc(sizeof(ListNode));
+        curr_node = curr_node->next;
+    }
+    
+    while(list2) {
+        curr_node->val = list2->val;
+        curr_node->next = malloc(sizeof(ListNode));
+        curr_node = curr_node->next;
+    }
+
 
     return merged_list;
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,16 @@ int main() {
     ListNode *list1, *list2, *merged_list;
     list1 = malloc(sizeof(ListNode));
     list2 = malloc(sizeof(ListNode));
+    int arr1[5] = {1, 4, -1, 2, 3};
+    int arr2[5] = {0, 2, -2, -4, 10};
 
+    for (int i = 0; i < 5; i++) {
+        list1->val = arr1[i];
+        list1->next = malloc(sizeof(ListNode));
+        list2->val = arr2[i];
+        list2->next = malloc(sizeof(ListNode));
+    }
+        
     merged_list = mergeTwoLists(list1, list2);
     
     print(merged_list);
